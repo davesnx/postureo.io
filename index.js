5227fd17558c1ea3685fb9a1cb8bb63d8113e946
David Sancho <dsnxmoreno@gmail.com>
2016-02-21 20:59:57 +0100

    Refactor the hole application in functional/Promises

--- index.js
+++ index.js
@@ -1,230 +1,104 @@
-var express       = require('express')
-var fs            = require('fs')
-var http          = require('http')
-var request       = require('request')
-var cheerio       = require('cheerio')
-var google        = require('google')
-var insta         = require('instagram-node').instagram({})
-var base64_decode = require('base64').decode
+require('dotenv').config()
+import request from 'request-promise'
+import cheerio from 'cheerio'
+import instagram from 'instagram-node'
+const insta = instagram.instagram()
 
-var instagrin = "https://instagr.in"
+const instagrin = 'https://instagr.in'
+const BLEND_PAGE_URL = `${instagrin}/blend/`
+// const querySiteInstagrin = 'site:instagr.in User Profile'
+// const Bing = require('node-bing-api')({ accKey: BING_API_KEY })
 
-// to implement: Proxy the google requests for bypassing Google restiction
-app.get('/scrape', function (req, res) {
+// const BING_API_KEY = process.env.BING_API_KEY
+const INSTAGRAM_UID = process.env.INSTAGRAM_UID
 
-    // The same search via CLI if install google in global mode
-    // "google -q 'site:instagr.in intext:recent?access_token -p 1 -n 10"
+// TODO: Create a database for save the accessToken
+// TODO: Create method to like media items https://github.com/mckelvey/instagram-node-lib
+// TODO: Search 'https://instagr.in/t/{:tag}' and getAccessToken
+// https://datamarket.azure.com/dataset/explore/bing/search
+// Bing.web(querySiteInstagrin, {
+//   top: 1, // Number of results (max 50)
+//   skip: 3 // Skip first 3 results
+// }, function (error, res, body) {
+//   if (error) console.log('error', error)
+//   console.log(body.d.results[0].Url)
+// })
 
-    google.resultsPerPage = 25
-    var nextCounter = 0
+function scrappAt (html) {
+  let ats = []
+  if (!html) throw Error('HTML not valid')
+  const $ = cheerio.load(html)
+  const nextUrlChildrens = $('.next_url').children()
+  if (nextUrlChildrens.length === 0) throw Error('Access Tokens not available')
+  nextUrlChildrens.each((i, el) => {
+    ats.push(getAccessTokenFromUrl($(el).text()))
+  })
+  return ats
+}
 
-    var query = 'site:"instagr.in" intext:"recent?access_token"'
+function getAccessTokenFromUrl (url) {
+  const rg = /access_token=(.*)\&count/
+  if (!rg.test(url)) throw Error('AccessTokenUrl isn\'t correct')
+  return rg.exec(url)[1]
+}
 
-    google(query, function (err, next, links) {
+function followUser (accessToken, userId) {
+  insta.use({
+    access_token: accessToken
+  })
+  insta.set_user_relationship(userId, 'follow', (err, result, remaining, limit) => {
+    if (err) console.log('Error ' + err.code + ': ' + err.error_message)
+    else console.log(result)
+  })
+}
 
-      if (err) { console.error(err.error_message) }
+function scrappBlendUrls (html) {
+  return new Promise((resolve, reject) => {
+    let blendUrls = []
+    const $ = cheerio.load(html)
+    $('.blend-title a').each((i, el) => {
+      blendUrls.push({
+        title: $(el).text(),
+        url: `${instagrin}${$(el).attr('href')}`
+      })
+    })
+    resolve(blendUrls)
+  })
+}
 
-      if (links) {
+function getBlendUrls (blendUrl) {
+  return new Promise((resolve, reject) => {
+    request(blendUrl)
+      .then((html) => resolve(html))
+      .catch((err) => reject(err))
+  })
+}
 
-        for (var i = 0 i < links.length ++i) {
+function followFromBlendUrl (url) {
+  return new Promise((resolve, reject) => {
+    request(url)
+      .then((html) => resolve(html))
+      .catch((err) => reject(err))
+  })
+}
 
-          console.log(links[i].link)
-
-          // Write links in .txt
-          fs.appendFile('txt/urls.txt', links[i].link + "\n", function(err){
-
+function main () {
+  getBlendUrls(BLEND_PAGE_URL)
+    .then((html) => scrappBlendUrls(html))
+    .then((blendUrls) => blendUrls.map((url) => followFromBlendUrl(url)))
+    .then((p) => {
+      p.map((r) => {
+        r.then((html) => {
+          scrappAt(html).map((token) => {
+            console.log('TOKEN ->', token)
+            setTimeout(() => {
+              followUser(token, INSTAGRAM_UID)
+            }, 3000)
           })
-        }
-
-        if (nextCounter < 4) {
-          nextCounter += 1
-          if (next) next()
-        }
-
-       }
-    })
-
-    res.end("Check console!")
-})
-
-app.get('/blend', function (req, res) {
-
-    // https://instagr.in/blend
-    var blend = instagrin + "/blend/"
-
-    request(blend, function (err, res, html) {
-
-        console.log("Requests all this links: ")
-
-        if (!err && res.statusCode == 200) {
-
-            var $ = cheerio.load(html)
-
-            // Get each blend url and request it.
-
-            $('.blend-title a').each( function (i, elem){
-
-                url = $(this).attr("href")
-
-                console.log(instagrin + url)
-
-                request(instagrin + '/' + url, function (err, res, html) {
-
-                    if (!err && res.statusCode == 200) {
-
-                        console.log("Save all this tokens on 'txt/tokens.txt'")
-
-                        var $ = cheerio.load(html)
-
-                        // Get each access_toekn
-
-                        $('.next_url span').each( function (i, elem){
-
-                            var at = /access_token=(.*)\&count/.exec( $(this).text() )
-                            if(at) at = at[1]
-
-                            console.log(at)
-
-                            fs.appendFile('txt/tokens.txt', at + "\n", function(err){
-
-                            })
-
-                        })
-                    } else console.log(err)
-                })
-            })
-
-        } else console.log(err)
-    })
-
-    res.end("Check console!")
-})
-
-app.get('/read/:what', function (res, req) {
-
-    var what = req.params.what
-
-    if(what == "urls") {
-
-        // Read .txt and for eachline call FollowInsta or FollowToken
-
-        fs.readFile('txt/' + what + '.txt', {encoding: 'utf-8', flag: 'rs'}, function(e, data) {
-            if (e) return console.log(e)
-            else { console.log("File readed.") }
-
-            data = data.split("\n").slice(0, -1)
-
-            for (var i in data) {
-
-                url = data[i]
-
-                console.log(i + " > " + url)
-
-                followInsta(url)
-            }
         })
-
-    } else if (what == "tokens") {
-
-        fs.readFile('txt/tokens.txt', {encoding: 'utf-8', flag: 'rs'}, function(e, data) {
-            if (e) return console.log(e)
-            else { console.log("File readed.") }
-
-            data = data.split("\n").slice(0, -1)
-
-            // For every line in txt file
-            for (var i in data) {
-
-                // get the url
-                url = data[i]
-                // Call follow with Token
-                followToken(url)
-            }
-        })
-    } else res.end("Bad :what parameter")
-
-    res.end("Check console and 'txt/" + what + ".txt'")
-
-})
-
-app.get('/readtokens', function (res, req) {
-
-    fs.readFile('txt/tokens.txt', {encoding: 'utf-8', flag: 'rs'}, function(e, data) {
-        if (e) return console.log(e)
-        else { console.log("File readed.") }
-
-        data = data.split("\n").slice(0, -1)
-
-        // For every line in txt file
-        for (var i in data) {
-
-            // get the url
-            url = data[i]
-            // Call follow with Token
-            followToken(url)
-        }
+      })
     })
-
-    res.end("Check console and 'txt/tokens.txt'")
-
-})
-
-
-function followInsta (url) {
-
-        // Request url and get at
-
-        request(url, function (err, res, html) {
-
-            if (!err && res.statusCode == 200) {
-
-                // Get with cheerio(jQuery for serverside)
-                // $('.next_url')
-
-                var $ = cheerio.load(html)
-
-                var access_token
-                $('.next_url').filter(function(){
-                       var data = $(this)
-                       access_token = data.text()
-                })
-
-                // Regex /=(.*)&c/ with base64 decode
-                var at = /=(.*)&c/.exec(base64_decode(access_token))
-                if(at) at = at[1]
-
-                // Call follow with Token
-                followToken(at)
-            }
-        })
+    .catch((err) => console.log(err))
 }
 
-// to implement: Proxy the instagram requests for bypassing 20req/h
-function followToken(at) {
-
-    if (at) {
-        // Access instagram api with 'at' access_token
-        insta.use({
-            access_token: at,
-        })
-
-        // Do the request via instagram api for follow {user_id}
-        insta.set_user_relationship('{user_id}', 'follow', function (err, result, remaining, limit) {
-            if(err) {
-                console.log("Error " + err.code + ": "  + err.error_message)
-            } else {
-                console.log("Result: ")
-                console.log(result)
-                console.log("-------")
-            }
-        })
-    }
-}
-
-var port = process.env.PORT || 3000
-
-app.listen(port, function () {
-  console.log('Magic happens on port %d', port)
-})
-
-exports = module.exports = app
+main()
